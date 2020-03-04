68dd6892b81420963d235b5844bd6ab9fd761de3
Panda (Thomas) Attwood | TickleThePanda <thomas.pg.attwood@gmail.com>
2020-03-04 12:37:09 +0000

    Fix login length

--- src/lambda/food-route.js
+++ src/lambda/food-route.js
@@ -83,7 +83,7 @@ app.post('/login/', async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     sameSite: true,
-    maxAge: (365 * 24 * 60 * 60)
+    maxAge: (365 * 24 * 60 * 60 * 1000)
   });
 
   res.redirect('/');
